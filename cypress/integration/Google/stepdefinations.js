c5f6466b730bef28cf4d554f0dd29fae7e9a5dc3
Chandrakanta <ch.ghadei@gmail.com>
2023-03-02 15:05:46 +0530

    updates

--- cypress/integration/Google/stepdefinations.js
+++ cypress/integration/Google/stepdefinations.js
@@ -1,12 +1,16 @@
 /// <reference types = "Cypress"/>
 
-import {Given, When, Then} from "cypress-cucumber-preprocessor/steps"
- 
+import {Given, When, Then} from "cypress-cucumber-preprocessor/steps";
+let data = require("../../fixtures/example.json");
+
+
 Given('Visit google home page',()=>{
+    cy.viewport(1024, 768);
     cy.visit("https://www.google.com/");
 })
 
 When("search tom and jerry" , ()=>{
-    cy.get("input[class='gLFyf']").type('tom and jerry');
+    // cy.get("input[class='gLFyf']").type('tom and jerry');
+    cy.get("input[class='gLFyf']").type(data.keywordsToSearch);
     cy.contains('Google Search').click();
 })
